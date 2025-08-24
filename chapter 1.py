c5647c06f3e2674ac6b460c26e1d04e6a2c2566f
akshitat1501-spec <akshita.t1501@gmail.com>
2025-08-24 22:11:08 +0530

    Add files via upload
    my first calculator.

---
+++ chapter 1.py
@@ -0,0 +1,16 @@
+number 1 = int(input("Enter the value of  first number"-))
+number 2 = int (input("Enter the value of second number"-))
+add = number 1+ number 2
+print("The sum of',number1,"and",number 2,"is",add")
+      sub = number1-number2
+print("The substraction of",number1,"and",number 2,"is", sub")
+    div = number 1 / number 2 
+print ("The division of",'number1,"and",number 2,"is",div")
+           flr = number1//number 2
+           print ("The floor division of",number1,"and", number 2,"is",flr)
+           exp= number1**number2
+           print "The exponential of"' number1,"and",number2,"is",exp)
+                 mod= number1%number2
+                 print("The module of",number1,"and",number2,"is",mod)
+print(":~)")
+          
